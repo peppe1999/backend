73f055b0a3ebfd4f0dbdcdd959caedbd797c2179
Giuseppe Scapaticci <Giuseppe.Scapaticci@eng.it>
2025-02-23 18:13:02 +0100

    add uml and doc api

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ##Installazione##
 Esegui il comando seguente per installare le dipendenze necessarie: pip install fastapi uvicorn pydantic
-Per avviare l'applicazione, spostati nella directory src e invia il comando uvicorn main:app --reload
+Per avviare l'applicazione, spostati nella directory backend del progetto e invia il comando uvicorn main:app --reload
 
 Una volta avviata, l'applicazione sarà accessibile all'indirizzo:
 http://127.0.0.1:8000/api/bookings  dove andremo a visualizzare tutte le prenotazioni che verranno effettuate
--- main.py
+++ main.py
@@ -25,17 +25,52 @@ bookings: List[Booking] = []
 
 @app.get("/api/bookings", response_model=List[Booking])
 def get_bookings():
+    """
+    _summary_: Visualizza tutte le prenotazioni disponibili
+
+    Returns:
+        List[Booking]: Una lista di tutte le prenotazioni esistenti
+    """
     return bookings
+
+
+
 
 @app.get("/api/bookings/{booking_id}", response_model=Booking)
 def get_booking(booking_id: int):
+    """
+    _summary_: Ottiene una prenotazione specifica in base all'ID
+
+    Args:
+        booking_id (int):  L'ID della prenotazione richiesta
+
+    Raises:
+        HTTPException: Se la prenotazione non viene trovata restituisce un errore 404
+
+    Returns:
+        Booking: L'oggetto prenotazione corrispondente all'ID specificato
+    """
     for booking in bookings:
         if booking.id == booking_id:
             return booking
     raise HTTPException(status_code=404, detail="Prenotazione non trovata")
 
+
+
+
 @app.post("/api/bookings", response_model=Booking)
 def create_booking(booking: Booking):
+    """_summary_: Crea una nuova prenotazione
+
+    Args:
+        booking (Booking): I dettagli della prenotazione da creare
+
+    Raises:
+        HTTPException: Se l'orario è già occupato restituisce un errore 400
+
+    Returns:
+        Booking: L'oggetto prenotazione appena creato
+    """
     for existing_booking in bookings:
         if existing_booking.date == booking.date and existing_booking.time == booking.time:
             raise HTTPException(status_code=400, detail="Orario già occupato")
@@ -43,8 +78,24 @@ def create_booking(booking: Booking):
     bookings.append(booking)
     return booking
 
+
+
+
 @app.put("/api/bookings/{booking_id}", response_model=Booking)
 def update_booking(booking_id: int, updated_booking: Booking):
+    """_summary_: Aggiorna una prenotazione esistente
+
+    Args:
+        booking_id (int): L'ID della prenotazione da aggiornare
+        updated_booking (Booking): I nuovi dettagli della prenotazione
+
+    Raises:
+        HTTPException: Se l'orario è già occupato da un'altra prenotazione restituisce un errore 400
+        HTTPException: Se la prenotazione non viene trovata restituisce un errore 404
+
+    Returns:
+         Booking: L'oggetto prenotazione aggiornato
+    """
     for booking in bookings:
         if booking.id == booking_id:
             for existing_booking in bookings:
@@ -61,9 +112,24 @@ def update_booking(booking_id: int, updated_booking: Booking):
             return booking
 
     raise HTTPException(status_code=404, detail="Prenotazione non trovata")
+    
+
+
+
 
 @app.delete("/api/bookings/{booking_id}")
 def delete_booking(booking_id: int):
+    """_summary_: Elimina una prenotazione esistente
+
+    Args:
+        booking_id (int): L'ID della prenotazione da eliminare
+
+    Raises:
+        HTTPException: Se la prenotazione non viene trovata restituisce un errore 404.
+
+    Returns:
+        dict: Messaggio di conferma dell'eliminazione
+    """
     for booking in bookings:
         if booking.id == booking_id:
             bookings.remove(booking)
